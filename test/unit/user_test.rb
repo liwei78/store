df0482380513c0435804e2b64153cb1dd2f621ce
liwei <hi@liwei.me>
2013-08-10 23:24:23 +0800

    fix some

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,8 +1,4 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  test "USER CREATE TRIGGER" do
-    User.create(email: 'xxx@123.com', password: '123456')
-    p ActiveRecord::Base.connection.select_all("select * from users_log")
-  end
 end
